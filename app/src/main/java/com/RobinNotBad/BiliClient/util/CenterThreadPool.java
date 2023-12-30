846346d2e0d46d9a64899c7570ce696bb45d34ea
SaigyoujiNexas <saigyoujinexas@gmail.com>
2023-12-30 12:48:45 +0800

    修改线程池

--- app/src/main/java/com/RobinNotBad/BiliClient/util/CenterThreadPool.java
+++ app/src/main/java/com/RobinNotBad/BiliClient/util/CenterThreadPool.java
@@ -1,7 +1,6 @@
 package com.RobinNotBad.BiliClient.util;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
@@ -11,14 +10,25 @@ import java.util.concurrent.atomic.AtomicReference;
  * 特性: 创建的线程60s后才销毁, 期间如果有其他的runnable需要运行, 直接用已创建的线程
  */
 public class CenterThreadPool {
-    private static AtomicReference<ExecutorService> INSTANCE;
+
+    private static AtomicReference<ExecutorService> INSTANCE = new AtomicReference<>();
     private static ExecutorService getInstance(){
         while(INSTANCE.get() == null){
-            INSTANCE.compareAndSet(null, Executors.newCachedThreadPool());
+            INSTANCE.compareAndSet(null, new ThreadPoolExecutor(
+                    1,
+                    Runtime.getRuntime().availableProcessors() * 2,
+                    60,
+                    TimeUnit.SECONDS,
+                    new ArrayBlockingQueue<>(10)
+
+
+            ));
         }
         return INSTANCE.get();
     }
     public static void run(Runnable runnable){
         getInstance().submit(runnable);
     }
+
+    public static requireNetWork(String url, )
 }
