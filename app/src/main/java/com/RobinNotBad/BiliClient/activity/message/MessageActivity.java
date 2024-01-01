2da2cd484026aeafa38d8d5d92ff993e840b4f2f
SaigyoujiNexas <saigyoujinexas@gmail.com>
2024-01-02 00:15:03 +0800

    CenterThreadPool没有多线程更新ui能力捏

--- app/src/main/java/com/RobinNotBad/BiliClient/activity/message/MessageActivity.java
+++ app/src/main/java/com/RobinNotBad/BiliClient/activity/message/MessageActivity.java
@@ -3,19 +3,15 @@ package com.RobinNotBad.BiliClient.activity.message;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import com.RobinNotBad.BiliClient.R;
 import com.RobinNotBad.BiliClient.activity.MenuActivity;
 import com.RobinNotBad.BiliClient.activity.base.BaseActivity;
-
 import com.RobinNotBad.BiliClient.api.MessageApi;
 import com.RobinNotBad.BiliClient.util.CenterThreadPool;
 import com.google.android.material.card.MaterialCardView;
-
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
 
 public class MessageActivity extends BaseActivity {
     public static MessageActivity instance = null;
@@ -42,7 +38,6 @@ public class MessageActivity extends BaseActivity {
         });
 
 
-        
         MaterialCardView like = findViewById(R.id.like);
         like.setOnClickListener(view -> {
             Intent intent = new Intent();
@@ -73,13 +68,17 @@ public class MessageActivity extends BaseActivity {
         CenterThreadPool.run(() -> {
             try {
                 JSONObject stats = MessageApi.getUnread();
-                ((TextView)findViewById(R.id.reply_text)).setText("回复我的(" + stats.getInt("reply") + "未读)");
-                ((TextView)findViewById(R.id.like_text)).setText("收到的赞(" + stats.getInt("like") + "未读)");
-                ((TextView)findViewById(R.id.at_text)).setText("@我(" + stats.getInt("at") + "未读)");
-                ((TextView)findViewById(R.id.system_text)).setText("系统通知(" + stats.getInt("system") + "未读)");
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            } catch (JSONException e) {
+                runOnUiThread(() -> {
+                    try {
+                        ((TextView) findViewById(R.id.reply_text)).setText("回复我的(" + stats.getInt("reply") + "未读)");
+                        ((TextView) findViewById(R.id.like_text)).setText("收到的赞(" + stats.getInt("like") + "未读)");
+                        ((TextView) findViewById(R.id.at_text)).setText("@我(" + stats.getInt("at") + "未读)");
+                        ((TextView) findViewById(R.id.system_text)).setText("系统通知(" + stats.getInt("system") + "未读)");
+                    }catch (Exception e){
+                        Toast.makeText(this, "解析消息数据失败!\n" + e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         });
