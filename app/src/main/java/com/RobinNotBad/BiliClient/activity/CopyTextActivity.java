dec1f18ee4b3541050b70a0825c2a585075a53a2
yograyfox <huicat28@163.com>
2024-01-19 23:44:40 +0800

    还是会导致卡死，抛弃

--- app/src/main/java/com/RobinNotBad/BiliClient/activity/CopyTextActivity.java
+++ app/src/main/java/com/RobinNotBad/BiliClient/activity/CopyTextActivity.java
@@ -6,16 +6,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.widget.EditText;
 import com.RobinNotBad.BiliClient.R;
 import com.RobinNotBad.BiliClient.activity.base.BaseActivity;
 import com.RobinNotBad.BiliClient.util.MsgUtil;
 
 public class CopyTextActivity extends BaseActivity {
-    
-    private boolean text_changing = false;
     private String content = "";
     
     @Override
@@ -31,23 +27,6 @@ public class CopyTextActivity extends BaseActivity {
 
         EditText edittext = findViewById(R.id.content);
         edittext.setText(content);
-        edittext.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if(!text_changing) edittext.setText(content);
-                text_changing = !text_changing;
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
         
         findViewById(R.id.copy_all).setOnClickListener(view -> {
             ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
