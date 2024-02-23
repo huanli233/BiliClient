21d2be5c5a678d54287db339e0bf9aafda45db97
SaigyoujiNexas <saigyoujinexas@gmail.com>
2024-02-23 21:22:49 +0800

    增加线程池的一些实现， 早晚用得到，如果现在用不到的话，正式包会有代码优化， 不用担心包体积会有膨胀喵

--- app/src/main/java/com/RobinNotBad/BiliClient/util/CenterThreadPool.java
+++ app/src/main/java/com/RobinNotBad/BiliClient/util/CenterThreadPool.java
@@ -1,19 +1,19 @@
 package com.RobinNotBad.BiliClient.util;
 
+import androidx.core.util.Consumer;
 import androidx.core.util.Supplier;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import com.bumptech.glide.util.Executors;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author silent碎月
  * 核心运行线程池, 最大线程容量为CPU核心数的2倍, 最小线程容量为1, 线程空闲时间为60s, 线程队列容量为20
+ * 注释掉的代码是由于当前第三方库的import不是很满足所以注掉了
+ * BuildersKt.launch 系列调用当引入kotlin协程库后可用于替代当前线程池的设计， 可以在java端调起协程
  */
 public class CenterThreadPool {
 
@@ -45,27 +45,67 @@ public class CenterThreadPool {
             runnable.run();
             return Unit.INSTANCE;
         });*/
-        getInstance().submit(runnable);
+        getInstance().execute(runnable);
     }
 
     /**
-     * 在后台运行, 用于网络请求等耗时操作, 有返回值, 使用LiveData.observe()获取返回值, 会自动切到主线程,不需要再runOnUiThread().
+     * 在后台运行, 用于网络请求等耗时操作, 有返回值,
+     * 在fragment, activity等位置使用LiveData.observe()获取返回值, 会自动切到主线程,不需要再runOnUiThread().
      * @param supplier 要运行的任务
      * @return LiveData包装的返回值
      * @param <T> 返回值类型
      */
-    public static <T> LiveData<T> supplyAsync(Supplier<T> supplier) {
+    public static <T> LiveData<T> supplyAsyncWithLiveData(Supplier<T> supplier) {
         MutableLiveData<T> retval = new MutableLiveData<>();
         /*BuildersKt.launch(INSTANCE, Dispatchers.getIO(), CoroutineStart.DEFAULT, (CoroutineScope scope, Continuation continuation) -> {
             T res = supplier.get();
             retval.postValue(res);
             return Unit.INSTANCE;
         });*/
-        getInstance().submit(() -> {
+        getInstance().execute(() -> {
             T res = supplier.get();
             retval.postValue(res);
         });
         return retval;
+    }
+
+    /**
+     * 在后台运行， 有返回值
+     * 使用 CenterThreadPool.observe方法对返回值进行观察
+     * @param supplier 一个带返回值的lambda表达式或Supplier的实现类
+     * @return 返回一个可供CenterThreadPool观察的Future对象
+     * @param <T> 返回值类型
+     */
+    public static <T> Future<T> supplyAsyncWithFuture(Supplier<T> supplier){
+        return getInstance().submit(supplier::get);
+    }
+
+    /**
+     * 对Future 对象进行观察， 无需切换线程， 自动在ui线程进行观察
+     * @param future 一个将要在未来返回一个 T 类型对象的对象
+     * @param consumer 对T进行观察的lambda表达式或者类
+     * @param <T> 要观察的类型
+     */
+    public static <T> void observe(Future<T> future, Consumer<T> consumer){
+        getInstance().execute(() -> {
+            try {
+                T res = future.get();
+                CenterThreadPool.runOnUiThread(() -> { consumer.accept(res); });
+            }catch(Exception ignored){
+            }
+        });
+    }
+
+
+    public static <T> void observe(Future<T> future, Consumer<T> consumer, Consumer<Throwable> onFailure){
+        getInstance().execute(() -> {
+            try {
+                T res = future.get();
+                CenterThreadPool.runOnUiThread(() -> { consumer.accept(res); });
+            }catch(Exception e){
+                onFailure.accept(e);
+            }
+        });
     }
 
     /**
