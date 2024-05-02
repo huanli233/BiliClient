12c13fa829957185d9c14981fd39d5e1b7911a24
RobinNotBad <robin_0229@qq.com>
2024-05-03 00:56:43 +0800

    完成动态页改造

--- app/src/main/java/com/RobinNotBad/BiliClient/activity/dynamic/DynamicActivity.java
+++ app/src/main/java/com/RobinNotBad/BiliClient/activity/dynamic/DynamicActivity.java
@@ -3,15 +3,8 @@ package com.RobinNotBad.BiliClient.activity.dynamic;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
-import com.RobinNotBad.BiliClient.R;
-import com.RobinNotBad.BiliClient.activity.base.InstanceActivity;
+import com.RobinNotBad.BiliClient.activity.base.RefreshMainActivity;
 import com.RobinNotBad.BiliClient.adapter.DynamicAdapter;
 import com.RobinNotBad.BiliClient.api.DynamicApi;
 import com.RobinNotBad.BiliClient.model.Dynamic;
@@ -24,28 +17,22 @@ import java.util.ArrayList;
 //动态页面
 //2023-09-17
 
-public class DynamicActivity extends InstanceActivity {
+public class DynamicActivity extends RefreshMainActivity {
 
-    private RecyclerView recyclerView;
-    private SwipeRefreshLayout swipeRefreshLayout;
     private ArrayList<Dynamic> dynamicList;
     private DynamicAdapter dynamicAdapter;
     private long offset = 0;
     private boolean firstRefresh = true;
-    private boolean refreshing = false;
-    private boolean bottom = false;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_simple_main_refresh);
         setMenuClick(4);
         Log.e("debug","进入动态页");
 
-        recyclerView = findViewById(R.id.recyclerView);
-        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
-        swipeRefreshLayout.setOnRefreshListener(this::refreshDynamic);
+        setOnRefreshListener(this::refreshDynamic);
+        setOnLoadMoreListener(page -> addDynamic());
 
         setPageName("动态");
 
@@ -59,70 +46,43 @@ public class DynamicActivity extends InstanceActivity {
 
     @SuppressLint("NotifyDataSetChanged")
     private void refreshDynamic() {
-        Log.e("debug","刷新");
-        CenterThreadPool.run(()->{
-            if (firstRefresh) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(DynamicActivity.this));
-                dynamicList = new ArrayList<>();
-                dynamicAdapter = new DynamicAdapter(this,dynamicList);
-                recyclerView.setAdapter(dynamicAdapter);
-            } else {
-                offset = 0;
-                bottom = false;
-                dynamicList.clear();
-                dynamicAdapter.notifyDataSetChanged();
-            }
-            runOnUiThread(() -> swipeRefreshLayout.setRefreshing(true));
+        Log.e("debug", "刷新");
+        if (firstRefresh) {
+            dynamicList = new ArrayList<>();
+        } else {
+            offset = 0;
+            bottom = false;
+            dynamicList.clear();
+            dynamicAdapter.notifyDataSetChanged();
+        }
 
-            refreshing = true;
-            addRecommend();
-
-        });
+        addDynamic();
     }
 
-    private void addRecommend() {
+    private void addDynamic() {
         Log.e("debug", "加载下一页");
-        runOnUiThread(()->swipeRefreshLayout.setRefreshing(true));
-        int lastSize = dynamicList.size();
-        try {
-            offset = DynamicApi.getDynamicList(dynamicList,offset,0);
-            bottom = (offset==-1);
+        CenterThreadPool.run(()->{
+            int lastSize = dynamicList.size();
+            try {
+                offset = DynamicApi.getDynamicList(dynamicList,offset,0);
+                bottom = (offset==-1);
+                setRefreshing(false);
 
-            runOnUiThread(() -> {
-                if (firstRefresh) {
-                    firstRefresh = false;
-                    recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-                        @Override
-                        public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
-                            super.onScrollStateChanged(recyclerView, newState);
-                        }
+                runOnUiThread(() -> {
+                    if (firstRefresh) {
+                        firstRefresh = false;
+                        dynamicAdapter = new DynamicAdapter(this,dynamicList);
+                        setAdapter(dynamicAdapter);
+                    }
+                    else {
+                        dynamicAdapter.notifyItemRangeInserted(lastSize, dynamicList.size() - lastSize);
+                    }
+                });
 
-                        @Override
-                        public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-                            super.onScrolled(recyclerView, dx, dy);
-                            LinearLayoutManager manager = (LinearLayoutManager) recyclerView.getLayoutManager();
-                            assert manager != null;
-                            int lastItemPosition = manager.findLastCompletelyVisibleItemPosition();  //获取最后一个完全显示的itemPosition
-                            int itemCount = manager.getItemCount();
-                            if (lastItemPosition >= (itemCount - 3) && dy > 0 && !refreshing && !bottom) {// 滑动到倒数第三个就可以刷新了
-                                refreshing = true;
-                                CenterThreadPool.run(() -> addRecommend()); //加载第二页
-                            }
-                        }
-                    });
-                }
-                dynamicAdapter.notifyItemRangeInserted(lastSize, dynamicList.size() - lastSize);
-                swipeRefreshLayout.setRefreshing(false);
-                refreshing = false;
-            });
-
-        } catch (Exception e) {
-            runOnUiThread(()->{
-                MsgUtil.err(e,this);
-                swipeRefreshLayout.setRefreshing(false);
-                refreshing = false;
-            });
-        }
+            } catch (Exception e) {
+                loadFail(e);
+            }
+        });
 
     }
 
