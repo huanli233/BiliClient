ae9b109cc766644978c6b9fcb5e36241ed66f310
ipmoye <huicat28@163.com>
2024-05-18 14:13:32 +0800

    复制文本页面可选择复制的范围

--- app/src/main/java/com/RobinNotBad/BiliClient/activity/CopyTextActivity.java
+++ app/src/main/java/com/RobinNotBad/BiliClient/activity/CopyTextActivity.java
@@ -5,6 +5,8 @@ import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 
 import com.RobinNotBad.BiliClient.R;
@@ -25,12 +27,79 @@ public class CopyTextActivity extends BaseActivity {
 
         EditText edittext = findViewById(R.id.content);
         edittext.setText(content);
-        
+
+        EditText beginEdit = findViewById(R.id.begin_index);
+        EditText endEdit = findViewById(R.id.end_index);
+
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                try {
+                    edittext.setSelection(Integer.parseInt(String.valueOf(beginEdit.getText())),Integer.parseInt(String.valueOf(endEdit.getText())));
+                }catch (Exception e){}
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        };
+        beginEdit.addTextChangedListener(textWatcher);
+        endEdit.addTextChangedListener(textWatcher);
+
         findViewById(R.id.copy_all).setOnClickListener(view -> {
             ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
             ClipData clipData = ClipData.newPlainText("label",content);
             clipboardManager.setPrimaryClip(clipData);
             MsgUtil.toast("已复制",this);
+        });
+        findViewById(R.id.copy).setOnClickListener(view -> {
+            try {
+                ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clipData = ClipData.newPlainText("label",content.substring(Integer.parseInt(String.valueOf(beginEdit.getText())),Integer.parseInt(String.valueOf(endEdit.getText()))));
+                clipboardManager.setPrimaryClip(clipData);
+                MsgUtil.toast("已复制",this);
+            }catch (Exception e){
+                MsgUtil.toast("复制失败，请检查范围",this);
+            }
+        });
+
+        findViewById(R.id.begin_left).setOnClickListener(view -> {
+            try {
+                if(Integer.parseInt(String.valueOf(beginEdit.getText())) - 1 < 0) beginEdit.setText("0");
+                else beginEdit.setText(String.valueOf(Integer.parseInt(String.valueOf(beginEdit.getText())) - 1));
+            }catch (Exception e){
+                beginEdit.setText("0");
+            }
+        });
+        findViewById(R.id.begin_right).setOnClickListener(view -> {
+            try {
+                if(Integer.parseInt(String.valueOf(beginEdit.getText())) + 1 > content.length() - 1) beginEdit.setText(String.valueOf(content.length()-1));
+                else beginEdit.setText(String.valueOf(Integer.parseInt(String.valueOf(beginEdit.getText())) + 1));
+            }catch (Exception e){
+                beginEdit.setText("0");
+            }
+        });
+        findViewById(R.id.end_left).setOnClickListener(view -> {
+            try {
+                if(Integer.parseInt(String.valueOf(endEdit.getText())) - 1 < 0) endEdit.setText("0");
+                else endEdit.setText(String.valueOf(Integer.parseInt(String.valueOf(endEdit.getText())) - 1));
+            }catch (Exception e){
+                endEdit.setText("0");
+            }
+        });
+        findViewById(R.id.end_right).setOnClickListener(view -> {
+            try {
+                if(Integer.parseInt(String.valueOf(endEdit.getText())) + 1 > content.length() - 1) endEdit.setText(String.valueOf(content.length()-1));
+                else endEdit.setText(String.valueOf(Integer.parseInt(String.valueOf(endEdit.getText())) + 1));
+            }catch (Exception e){
+                endEdit.setText("0");
+            }
         });
     }
 }
