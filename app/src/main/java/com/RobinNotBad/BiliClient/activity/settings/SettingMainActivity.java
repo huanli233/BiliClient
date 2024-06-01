d99c43ffb8a242c9ff0aad1b5d25267bd664b652
ipmoye <huicat28@163.com>
2024-06-01 14:38:41 +0800

    修改版本号

--- app/src/main/java/com/RobinNotBad/BiliClient/activity/settings/SettingMainActivity.java
+++ app/src/main/java/com/RobinNotBad/BiliClient/activity/settings/SettingMainActivity.java
@@ -147,9 +147,9 @@ public class SettingMainActivity extends InstanceActivity {
 
 
         MaterialCardView test = findViewById(R.id.test);    //用于测试
-//        test.setVisibility(View.GONE);
+        test.setVisibility(View.GONE);
         test.setOnClickListener(view -> {
-            throw new OutOfMemoryError("测试");
+//            throw new OutOfMemoryError("测试");
             //MsgUtil.toast("喵~",this)
             //MsgUtil.showText(this,"test","你干嘛~哈哈~哎哟<extra_insert>{\"type\":\"video\",\"content\":\"BV1Bo4y1c7gW\",\"title\":\"纳西妲纳西妲纳西妲嘿嘿嘿\"}");
         });
