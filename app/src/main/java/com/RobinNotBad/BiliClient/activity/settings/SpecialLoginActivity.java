f15a267fd47526bbc78a11418db9f4a5ba00db13
RobinNotBad <robin_0229@qq.com>
2024-06-13 11:36:13 +0800

    修改登录页布局和文本，修改了彩蛋文本

--- app/src/main/java/com/RobinNotBad/BiliClient/activity/settings/SpecialLoginActivity.java
+++ app/src/main/java/com/RobinNotBad/BiliClient/activity/settings/SpecialLoginActivity.java
@@ -62,10 +62,11 @@ public class SpecialLoginActivity extends BaseActivity {
             });
         }
         else{
-            desc.setText("以下是你的登录信息。此功能是为低版本安卓用户无法登录的问题准备，如果你是意外进入此页面，请退出。\n同时，请勿将此页面的信息传给别人，以防盗号！");
-            ((TextView) findViewById(R.id.textView)).setText("复制");
-            ((TextView) findViewById(R.id.textView)).setCompoundDrawables(null, null, null, null);
-            ((TextView) findViewById(R.id.textView)).setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            desc.setText(R.string.special_login_export);
+            TextView buttonText = findViewById(R.id.confirm_text);
+            buttonText.setText("复制");
+            buttonText.setCompoundDrawables(null, null, null, null);
+            buttonText.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
             JSONObject jsonObject = new JSONObject();
             try {
                 jsonObject.put("cookies",SharedPreferencesUtil.getString("cookies",""));
@@ -74,7 +75,7 @@ public class SpecialLoginActivity extends BaseActivity {
                 e.printStackTrace();
             }
             textInput.setText(jsonObject.toString());
-            findViewById(R.id.textView).setOnClickListener((view) -> {
+            confirm.setOnClickListener((view) -> {
                 ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
                 ClipData clipData = ClipData.newPlainText("label", textInput.getText());
                 clipboardManager.setPrimaryClip(clipData);
