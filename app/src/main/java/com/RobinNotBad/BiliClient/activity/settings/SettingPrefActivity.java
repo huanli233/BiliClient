838e4c2e80fee5f6705c499f86f0805228fc4c7e
huanli233 <p2720791562@outlook.com>
2024-06-22 23:29:31 +0800

    视频详情页添加progressPair==null判断；修复dynamic_action直播按钮布局错误

--- app/src/main/java/com/RobinNotBad/BiliClient/activity/settings/SettingPrefActivity.java
+++ app/src/main/java/com/RobinNotBad/BiliClient/activity/settings/SettingPrefActivity.java
@@ -15,14 +15,6 @@ import java.util.List;
 
 public class SettingPrefActivity extends RefreshListActivity {
 
-    /*
-    private SwitchMaterial back_disable,fav_single,fav_notice, video_tags, video_related,
-            myspace_creativecenter,
-            copy_enable, cover_play_enable,image_no_load_onscroll,
-            text_link_enable, disable_network_check;
-
-     */
-
     @SuppressLint({"MissingInflatedId", "SetTextI18n", "InflateParams"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,64 +43,6 @@ public class SettingPrefActivity extends RefreshListActivity {
 
         setRefreshing(false);
 
-        /*
-        back_disable = findViewById(R.id.back_disable);
-        back_disable.setChecked(SharedPreferencesUtil.getBoolean("back_disable", false));
-
-        fav_single = findViewById(R.id.fav_single);
-        fav_single.setChecked(SharedPreferencesUtil.getBoolean("fav_single", false));
-
-        fav_notice = findViewById(R.id.fav_notice);
-        fav_notice.setChecked(SharedPreferencesUtil.getBoolean("fav_notice", false));
-
-        video_tags = findViewById(R.id.tags_enable);
-        video_tags.setChecked(SharedPreferencesUtil.getBoolean("tags_enable", true));
-
-        video_related = findViewById(R.id.related_enable);
-        video_related.setChecked(SharedPreferencesUtil.getBoolean("related_enable", true));
-
-        myspace_creativecenter = findViewById(R.id.creative_enable);
-        myspace_creativecenter.setChecked(SharedPreferencesUtil.getBoolean("creative_enable", true));
-
-        copy_enable = findViewById(R.id.copy_enable);
-        copy_enable.setChecked(SharedPreferencesUtil.getBoolean("copy_enable", true));
-
-        cover_play_enable = findViewById(R.id.cover_play_enable);
-        cover_play_enable.setChecked(SharedPreferencesUtil.getBoolean("cover_play_enable", false));
-
-        image_no_load_onscroll = findViewById(R.id.image_no_load_onscroll);
-        image_no_load_onscroll.setChecked(SharedPreferencesUtil.getBoolean("image_no_load_onscroll", true));
-
-        text_link_enable = findViewById(R.id.text_link_enable);
-        text_link_enable.setChecked(SharedPreferencesUtil.getBoolean("link_enable", true));
-
-        disable_network_check = findViewById(R.id.disable_network_check);
-        disable_network_check.setChecked(SharedPreferencesUtil.getBoolean("network_check_disable", false));
-
-
-         */
     }
 
-    /*
-    private void save() {
-        SharedPreferencesUtil.putBoolean("back_disable", back_disable.isChecked());
-        SharedPreferencesUtil.putBoolean("fav_single", fav_single.isChecked());
-        SharedPreferencesUtil.putBoolean("fav_notice", fav_notice.isChecked());
-        SharedPreferencesUtil.putBoolean("tags_enable", video_tags.isChecked());
-        SharedPreferencesUtil.putBoolean("related_enable", video_related.isChecked());
-        SharedPreferencesUtil.putBoolean("creative_enable", myspace_creativecenter.isChecked());
-        SharedPreferencesUtil.putBoolean("copy_enable", copy_enable.isChecked());
-        SharedPreferencesUtil.putBoolean("cover_play_enable", cover_play_enable.isChecked());
-        SharedPreferencesUtil.putBoolean("image_no_load_onscroll", image_no_load_onscroll.isChecked());
-        SharedPreferencesUtil.putBoolean("link_enable", text_link_enable.isChecked());
-        SharedPreferencesUtil.putBoolean("network_check_disable", disable_network_check.isChecked());
-    }
-
-    @Override
-    protected void onDestroy() {
-        //save();
-        super.onDestroy();
-    }
-
-     */
 }
