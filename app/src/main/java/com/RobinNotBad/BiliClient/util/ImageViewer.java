d9b10268c51a7c3c0bb66fc177c20fc8e80941f9
huanli233 <p2720791562@outlook.com>
2024-08-19 11:05:21 +0800

    封装ImageViewer

---
+++ app/src/main/java/com/RobinNotBad/BiliClient/util/ImageViewer.java
@@ -0,0 +1,33 @@
+package com.RobinNotBad.BiliClient.util;
+
+import android.content.Intent;
+import android.view.View;
+
+import com.RobinNotBad.BiliClient.activity.ImageViewerActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImageViewer {
+    public static ImageViewer from(String url) {
+        return new ImageViewer(new ArrayList<>(List.of(url)));
+    }
+    public static ImageViewer from(List<String> urls) {
+        return new ImageViewer(new ArrayList<>(urls));
+    }
+
+    private final ArrayList<String> urlList;
+
+    private ImageViewer(ArrayList<String> urlList) {
+        this.urlList = urlList;
+    }
+
+    public void into(View view) {
+        view.setOnClickListener((v) -> {
+            Intent intent = new Intent();
+            intent.setClass(v.getContext(), ImageViewerActivity.class);
+            intent.putExtra("imageList", urlList);
+            v.getContext().startActivity(intent);
+        });
+    }
+}
